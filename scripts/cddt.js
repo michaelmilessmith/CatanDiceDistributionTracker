abc4642169f9fdebce7c1a74d019888a77c3750d
michaelmilessmith <michaelmilessmith@gmail.com>
2016-12-02 16:49:03 +0000

    Changes charts to proportional representation of the highest count rather than total

--- scripts/cddt.js
+++ scripts/cddt.js
@@ -1,7 +1,7 @@
 const Bar = React.createClass({
   render: function() {
   	// var bar = Array(this.props.count + 1).join("-");
-    const classes = `bar bar-${Math.round((this.props.count * 100) / this.props.total)}`
+    const classes = `bar bar-${Math.round((this.props.count * 100) / this.props.highest)}`
     return (<div className={classes}><span className="bar-label">{this.props.count}</span></div>);
   }
 });
@@ -22,7 +22,7 @@ const NumberBox = React.createClass({
   render: function() {
     return <div>
       <Number  onClick={this.handleClick} number={this.props.object.number} />
-      < Bar count={this.props.object.count} total={this.props.total} />
+      < Bar count={this.props.object.count} total={this.props.total} highest={this.props.highest} />
     </div>;
   }
 });
@@ -40,10 +40,11 @@ const Chart = React.createClass({
 },
   render: function() {
     const totalCount = this.state.numbers.reduce((a, b) => a + b.count, 0);
+    const highestCount = this.state.numbers.reduce((a, b) => a > b.count ? a : b.count, 0);
     console.log(totalCount);
     return (
       <div>
-      	{this.state.numbers.map( object => <NumberBox object={object} onClick={this.handlePlus} total={totalCount}/> )}
+      	{this.state.numbers.map( object => <NumberBox object={object} onClick={this.handlePlus} total={totalCount} highest={highestCount}/> )}
         </div>);
   }
 });
